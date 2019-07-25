1464e1c489bbdb778c8ae0e8ecba6bee8a7ce977
wangtao455494922 <455494922@qq.com>
2019-07-25 15:54:21 +0800

    Eureka

--- springcloud-provider-product/src/main/java/wjt/ProviderApplication.java
+++ springcloud-provider-product/src/main/java/wjt/ProviderApplication.java
@@ -3,9 +3,11 @@ package wjt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("wjt.mapper")
+@EnableEurekaClient
 public class ProviderApplication
 {
     public static void main(String[] args) {
